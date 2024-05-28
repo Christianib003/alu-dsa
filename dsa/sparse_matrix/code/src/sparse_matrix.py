7617d20e66a390cea025c995ee16a8bf1d461fe4
Christianib003 <christianib003@gmail.com>
2024-05-28 12:23:45 +0200

    add __init__ method in SparseMatrix

--- dsa/sparse_matrix/code/src/sparse_matrix.py
+++ dsa/sparse_matrix/code/src/sparse_matrix.py
@@ -9,4 +9,10 @@ class SparseMatrix:
     """
     A class used to represent a Sparse Matrix
     """
-    pass
+    def __init__(self, matrix_file_path=None, num_rows=None, num_cols=None):
+        """
+        Initialize the SparseMatrix object
+        """
+        self.num_rows = num_rows
+        self.num_cols = num_cols
+        self.elements = {}
