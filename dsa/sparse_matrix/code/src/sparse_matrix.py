1d36b6ea6c10f3b8c5eb2062a445ebdcc4da8780
Christianib003 <christianib003@gmail.com>
2024-05-28 12:34:21 +0200

    add method to load matrix form a file

--- dsa/sparse_matrix/code/src/sparse_matrix.py
+++ dsa/sparse_matrix/code/src/sparse_matrix.py
@@ -16,3 +16,28 @@ class SparseMatrix:
         self.num_rows = num_rows
         self.num_cols = num_cols
         self.elements = {}
+        if matrix_file_path:
+            self.load_matrix(matrix_file_path)
+
+    def load_matrix(self, matrix_file_path):
+        """
+        Load the matrix from a file
+
+        Args:
+            matrix_file_path: (_type_): _description_
+
+        Raises:
+            ValueError: Raise an error if the input file has wrong format
+        """
+        try:
+            with open(matrix_file_path, 'r', encoding='utf-8') as file:
+                lines = file.readlines()
+                self.num_rows = int(lines[0].split('=')[1].strip())
+                self.num_cols = int(lines[1].split('=')[1].strip())
+                for line in lines[2:]:
+                    line = line.strip()
+                    if line:
+                        row, col, value = map(int, line.strip('()').split(','))
+                        self.elements[(row, col)] = value
+        except ValueError as e:
+            raise ValueError("Input file has wrong format") from e
