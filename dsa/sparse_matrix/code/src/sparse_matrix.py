302aaca2e7a63f029e22fe7c85f235430bfa0a1e
Christianib003 <christianib003@gmail.com>
2024-05-28 12:21:00 +0200

    add module and class documentation

--- dsa/sparse_matrix/code/src/sparse_matrix.py
+++ dsa/sparse_matrix/code/src/sparse_matrix.py
@@ -1,4 +1,12 @@
 #! /usr/bin/python3
+"""
+A module holding a class representing a Sparse Matrix
+operations
+"""
+
 
 class SparseMatrix:
+    """
+    A class used to represent a Sparse Matrix
+    """
     pass
